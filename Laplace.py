2e6c20fa75983a94c536d497d6660d4f76ef3aa8
Sushi <82881682+BlezenDragon@users.noreply.github.com>
2023-10-14 14:08:21 +0530

    Update Laplace.py

--- Laplace.py
+++ Laplace.py
@@ -1,7 +1,6 @@
 import sympy
 from sympy import pretty_print
 t, s = sympy.symbols('t s')
-# Get user input for the function
 Sfunc = input("Enter the function in terms of t: ")
 f1 = sympy.sympify(Sfunc)
 Laplace_f1 = sympy.laplace_transform(f1, t, s, noconds=True)
