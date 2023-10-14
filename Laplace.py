f24537552d22b712e4380c4fd719dcae115bbc9a
Sushi <82881682+BlezenDragon@users.noreply.github.com>
2023-10-14 10:57:45 +0530

    finallaplace

--- Laplace.py
+++ Laplace.py
@@ -1,6 +1,7 @@
 import sympy
+from sympy import pretty_print
 t, s = sympy.symbols('t s')
 Tfunc = input("Enter the time varying function: ")
 f1 = sympy.sympify(Tfunc)
 Laplacef1 = sympy.laplace_transform(f1, t, s, noconds=True)
-print("Laplace Transform of", Tfunc, "is", Laplacef1)
+pretty_print(Laplacef1)
